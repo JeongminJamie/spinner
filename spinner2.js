87460997ab6fe1d2e26bbcad684288103593d18d
JeongminJamie <sksrhcalsu@gmail.com>
2022-05-10 16:44:33 -0400

    spinner 2

---
+++ spinner2.js
@@ -0,0 +1,11 @@
+const start = ['\r|   ', '\r/   ', '\r-   ', '\r\\   ', '\r-   ', '\r\\   ', '\r|   '];
+
+let time = 100;
+for (let element of start) {
+setTimeout(() => {
+  process.stdout.write(element);
+}, time);
+time += 200;
+};
+
+
